2b497f26c3567c1c8098902383d9c355eccc3007
Kamran <41294454+kamrans262@users.noreply.github.com>
2026-09-13 04:24:35 +0500

    fix: protect Admin Maker media routes from non-makers

--- app/Features/Admin/Makers/Http/Controllers/DeleteProfileImageController.php
+++ app/Features/Admin/Makers/Http/Controllers/DeleteProfileImageController.php
@@ -3,14 +3,20 @@
 namespace App\Features\Admin\Makers\Http\Controllers;
 
 use App\Features\Account\Actions\DeleteMakerProfileImageAction;
+use App\Features\Auth\Enums\UserRole;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\RedirectResponse;
 
 final class DeleteProfileImageController extends Controller
 {
     public function __invoke(User $maker, DeleteMakerProfileImageAction $action): RedirectResponse
     {
+        if (! $maker->hasRole(UserRole::Maker)) {
+            throw (new ModelNotFoundException())->setModel(User::class, [$maker->getKey()]);
+        }
+
         $action->execute($maker);
 
         return redirect()
